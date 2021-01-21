770b5d769d30fab662e7e927bf9aed86f8af8c34
yogesh <yogeshsys05@gmail.com>
2021-01-21 17:58:58 +0000

    Change Version V1.0

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
         { title: 'another event', id: 2, description: 'something even cooler' },
-        { title: 'latest event', id: 3, description: 'food and fun!' }
+        { title: 'latest event', id: 3, description: 'food and fun changed!' }
     ]
 };
 
